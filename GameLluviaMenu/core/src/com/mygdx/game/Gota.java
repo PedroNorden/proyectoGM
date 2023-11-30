72e8f793681eaf69fff84be4a3d2d42dcfcd62c1
Ignacio Ávila <127423265+nachoavila@users.noreply.github.com>
2023-11-29 23:09:11 -0400

    sonido añadido al constructor

--- GameLluviaMenu/core/src/com/mygdx/game/Gota.java
+++ GameLluviaMenu/core/src/com/mygdx/game/Gota.java
@@ -1,20 +1,27 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
 
-public abstract class Gota implements Colisionable{
-    protected Rectangle position;
-    protected Texture texture;
+public abstract class Gota{
+    private Rectangle position;
+    private Texture texture;
+    private Sound sonido;
 
-    public Gota(Texture texture, float x, float y) {
+    public Gota(Texture texture, float x, float y, Sound sonido) {
         this.texture = texture;
         this.position = new Rectangle(x, y, 34, 52);
+        this.sonido = sonido;
     }
 
     public Rectangle getPosition() {
         return position;
+    }
+
+    public Sound getSonido() {
+        return sonido;
     }
 
     public abstract void actuar(Tarro tarro);
@@ -22,7 +29,8 @@ public abstract class Gota implements Colisionable{
     public void dibujar(SpriteBatch batch) {
         batch.draw(texture, position.x, position.y);
     }
-    
+
+
     public abstract boolean colisionaCon(Tarro objeto);
-    
+
 }
